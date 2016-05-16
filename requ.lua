f333d63cef786c28623bdbd9016eebfc3c308224
Feynman Liang <feynman.liang@gmail.com>
2016-05-16 18:23:12 +0100

    Implements ReQU

--- requ.lua
+++ requ.lua
@@ -5,14 +5,15 @@ local ReQU = torch.class('nn.ReQU', 'nn.Module')
 function ReQU:updateOutput(input)
   -- TODO
   self.output:resizeAs(input):copy(input)
-  -- ...something here...
+  self.output:clamp(0, math.huge):pow(2)
   return self.output
 end
 
 function ReQU:updateGradInput(input, gradOutput)
   -- TODO
   self.gradInput:resizeAs(gradOutput):copy(gradOutput)
-  -- ...something here...
+  input:clamp(0, math.huge):mul(2)
+  self.gradInput:cmul(input)
   return self.gradInput
 end
 
